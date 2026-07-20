2f5f266c1ef2c76624418834c6e11e46a705a559
ThurayaKhaled <Thurayak184@gmail.com>
2026-07-20 11:56:57 +0300

    part 201

--- 06-spring-boot-mvc/01-ThymeleafDemo/ThymeleafDemo/src/main/java/com/example/SpringBoot/ThymeleafDemo/controller/StudentController.java
+++ 06-spring-boot-mvc/01-ThymeleafDemo/ThymeleafDemo/src/main/java/com/example/SpringBoot/ThymeleafDemo/controller/StudentController.java
@@ -1,18 +1,35 @@
 package com.example.SpringBoot.ThymeleafDemo.controller;
 
 import com.example.SpringBoot.ThymeleafDemo.model.Student;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.List;
+
 @Controller
 public class StudentController {
+
+    @Value("${countries}")
+    private List<String> countries;
+
+    @Value("${PLanguage}")
+    private List<String>PLanguage;
+
+    @Value("${Systems}")
+    private List<String>Systems;
+
     @GetMapping("/showStudentForm")
     public String showForm(Model model) {
         Student student = new Student();
         model.addAttribute("student", student);
+        model.addAttribute("countries", countries);
+        model.addAttribute("PLanguage", PLanguage);
+        model.addAttribute("Systems", Systems);
+
         return "studentForm";
 
     }
--- 06-spring-boot-mvc/01-ThymeleafDemo/ThymeleafDemo/src/main/java/com/example/SpringBoot/ThymeleafDemo/model/Student.java
+++ 06-spring-boot-mvc/01-ThymeleafDemo/ThymeleafDemo/src/main/java/com/example/SpringBoot/ThymeleafDemo/model/Student.java
@@ -1,8 +1,13 @@
 package com.example.SpringBoot.ThymeleafDemo.model;
 
+import java.util.List;
+
 public class Student {
     private String firstName;
     private String lastName;
+    private String country;
+    private String favLanguage;
+    private List<String> favSystem;
     public Student(){
 
     }
@@ -10,6 +15,27 @@ public class Student {
     public Student(String firstName, String lastName) {
         this.firstName = firstName;
         this.lastName = lastName;
+    }
+
+    public Student(String firstName, String lastName, String country) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.country = country;
+    }
+
+    public Student(String firstName, String lastName, String country, String favLanguage) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.country = country;
+        this.favLanguage = favLanguage;
+    }
+
+    public Student(String firstName, String lastName, String country, String favLanguage, List<String> favSystem) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.country = country;
+        this.favLanguage = favLanguage;
+        this.favSystem = favSystem;
     }
 
     public String getFirstName() {
@@ -26,5 +52,29 @@ public class Student {
 
     public void setLastName(String lastName) {
         this.lastName = lastName;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getFavLanguage() {
+        return favLanguage;
+    }
+
+    public void setFavLanguage(String favLanguage) {
+        this.favLanguage = favLanguage;
+    }
+
+    public List<String> getFavSystem() {
+        return favSystem;
+    }
+
+    public void setFavSystem(List<String> favSystem) {
+        this.favSystem = favSystem;
     }
 }
